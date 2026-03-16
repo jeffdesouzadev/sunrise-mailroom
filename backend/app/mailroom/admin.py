6e9e0e02ab53f3a4b47a2fec7640abdc2edfde44
Jeff DeSouza <jdesouza@rapidascent.io>
2026-03-15 19:32:37 -0500

    Add package model and packages API

--- backend/app/mailroom/admin.py
+++ backend/app/mailroom/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Package
 
-# Register your models here.
+
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ("recipient_name", "carrier", "status", "received_at", "picked_up_at")
+    search_fields = ("recipient_name", "tracking_number", "carrier")
+    list_filter = ("status", "carrier")
