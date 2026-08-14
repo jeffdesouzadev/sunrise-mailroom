f6dd04676d537ebe3654403e099f875b68103c62
Jeff DeSouza <jdesouza@rapidascent.io>
2026-08-14 01:31:07 -0500

    Prepare app for Windows packaging

--- backend/app/launcher.py
+++ backend/app/launcher.py
@@ -14,17 +14,28 @@ import django
 
 django.setup()
 
+from django.core.management import call_command
 from config.wsgi import application
 
 
 def open_browser():
     time.sleep(1.5)
+
     webbrowser.open(
         "http://127.0.0.1:8765"
     )
 
 
 if __name__ == "__main__":
+    #
+    # Initialize or upgrade the persistent SQLite database.
+    #
+    call_command(
+        "migrate",
+        interactive=False,
+        verbosity=1,
+    )
+
     threading.Thread(
         target=open_browser,
         daemon=True,
