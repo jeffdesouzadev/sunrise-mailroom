3d0b99c1e56f0ca5bed7cc5ba80556f7bc72d007
J. F. DeSouza <jeffdesouzadev@gmail.com>
2026-08-14 00:50:47 -0500

    api base changed for packaging

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 
-const API_BASE = "http://127.0.0.1:8000/api";
+// const API_BASE = "http://127.0.0.1:8000/api";
+const API_BASE = "/api";
 const currentYear = new Date().getFullYear();
 
 
